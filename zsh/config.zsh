44d8c7654b389eaecd7f144eef31ba0dda943ba0
James Conroy-Finn <james@logi.cl>
2011-11-19 11:14:37 +0000

    Remove duplicate extended_history from zsh conf

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -41,7 +41,6 @@ setopt ignore_eof
 setopt alwaystoend      # when complete from middle, move cursor
 
 setopt append_history         # adds history
-setopt extended_history
 setopt extended_history       # add timestamps to history
 setopt hist_expire_dups_first
 setopt hist_ignore_all_dups   # don't record dupes in history
