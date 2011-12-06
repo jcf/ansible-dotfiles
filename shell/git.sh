2a6ee2c3217f7b4e9e0afa222b4988c2495547e5
James Conroy-Finn <james@logi.cl>
2011-12-06 20:37:32 +0000

    Change git log aliases
     - glg generates a decorated oneline log
     - gll generates a decorated graph of branches and merges

--- shell/git.sh
+++ shell/git.sh
@@ -10,10 +10,13 @@ alias gca='git commit -av'
 alias gco='git checkout'
 alias gb='git branch -v'
 alias gs='git status -sb'
+
 alias glog='git log --stat'
+alias glg="git log --decorate --oneline"
+alias gll="git log --decorate --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
+
 alias gmt='git mergetool'
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
-alias glg="git log --decorate --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias ignore_empty='find . \( -type d -empty \) -and \( -not -regex ./\.git.* \) -exec touch {}/.gitignore \;'
 
 if [[ -d "/usr/local/git" ]]; then
