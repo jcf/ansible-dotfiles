4b6b8c48d85adec872ab0eb0a586ad6e18cb57ba
James Conroy-Finn <james@logi.cl>
2012-06-21 19:17:44 +0100

    Explain the complicated rbenv setup process

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,3 +1,8 @@
+# When using rbenv on a chef client with the rbenv cookbook from
+# https://github.com/fnichol/chef-rbenv, this is the way to make sure rbenv is
+# available within interactive shells.
+#
+# If this is not done, rbenv will not be available via PATH.
 if [[ -s "${HOME}/.rbenv/bin" ]]; then
   rbenv_root="${HOME}/.rbenv"
 else
