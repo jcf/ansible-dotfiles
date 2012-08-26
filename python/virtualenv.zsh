b04b9f0153563710febc16aa53113a459821addb
James Conroy-Finn <james@logi.cl>
2012-08-26 14:45:16 +0100

    Source virtualenvwrapper.sh if it exists

---
+++ python/virtualenv.zsh
@@ -0,0 +1,10 @@
+# virtualenvwrapper
+#
+# Installed via easy_install and pip as root:
+#
+#   sudo easy_install pip
+#   sudo pip install virtualenv
+#   sudo pip install virtualenvwrapper
+if [[ -f /usr/local/bin/virtualenvwrapper.sh ]]; then
+  source /usr/local/bin/virtualenvwrapper.sh
+fi
