ea4bb53910104710ec1ffcf97ee03ed2c45d1230
James Conroy-Finn <james@logi.cl>
2012-08-28 10:26:24 +0100

    Remove use of rbenv_root from rbenv.zsh

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,14 +1,2 @@
-# When using rbenv on a chef client with the rbenv cookbook from
-# https://github.com/fnichol/chef-rbenv, this is the way to make sure rbenv is
-# available within interactive shells.
-#
-# If this is not done, rbenv will not be available via PATH.
-if [[ -s "${HOME}/.rbenv/bin" ]]; then
-  rbenv_root="${HOME}/.rbenv"
-else
-  rbenv_root="/usr/local/rbenv"
-  export RBENV_ROOT="$rbenv_root"
-fi
-
-export PATH="${rbenv_root}/bin:$PATH"
+path=($HOME/.rbenv/bin $path)
 eval "$(rbenv init -)"
