9593bfe9bf5b170e37ed8961994d0363f3605695
James Conroy-Finn <james@logi.cl>
2012-09-09 20:13:18 +0100

    Remove --1.9 from JRUBY_OPTS
    It's the default now.

--- jruby/options.zsh
+++ jruby/options.zsh
@@ -1,6 +1,3 @@
-# Ruby JRuby in 1.9 mode
-export JRUBY_OPTS='--1.9'
-
 # Use custom Java 7 install (http://goo.gl/MkCSa)
 if [[ -d "/Library/Java/JavaVirtualMachines/jdk1.7.0_07.jdk/Contents/Home" ]]; then
   export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_07.jdk/Contents/Home"
