a7912f387598a0b1e4736310251d1544e5175749
James Conroy-Finn <james@logi.cl>
2012-09-09 19:13:30 +0100

    Set JAVA_HOME when 1.7.0 install is present

--- jruby/options.zsh
+++ jruby/options.zsh
@@ -3,3 +3,8 @@ export JAVA_OPTS='-d32'
 
 # Ruby JRuby in 1.9 mode
 export JRUBY_OPTS='--1.9'
+
+# Use custom Java 7 install (http://goo.gl/P8gO1)
+if [[ -d "/Library/Java/JavaVirtualMachines/1.7.0.jdk/Contents/Home" ]]; then
+  export JAVA_HOME="/Library/Java/JavaVirtualMachines/1.7.0.jdk/Contents/Home"
+fi
