43a8ff8be6552ecb5c0eb284b6bc06ce9c7d7210
James Conroy-Finn <james@logi.cl>
2012-09-09 20:01:22 +0100

    Use Java 7 server in 64bit mode

--- jruby/options.zsh
+++ jruby/options.zsh
@@ -1,10 +1,8 @@
-# Use the client JVM (http://j.mp/NHv51E)
-export JAVA_OPTS='-d32'
-
 # Ruby JRuby in 1.9 mode
 export JRUBY_OPTS='--1.9'
 
-# Use custom Java 7 install (http://goo.gl/P8gO1)
-if [[ -d "/Library/Java/JavaVirtualMachines/1.7.0.jdk/Contents/Home" ]]; then
-  export JAVA_HOME="/Library/Java/JavaVirtualMachines/1.7.0.jdk/Contents/Home"
+# Use custom Java 7 install (http://goo.gl/MkCSa)
+if [[ -d "/Library/Java/JavaVirtualMachines/jdk1.7.0_07.jdk/Contents/Home" ]]; then
+  export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_07.jdk/Contents/Home"
+  path=($JAVA_HOME/bin $path)
 fi
