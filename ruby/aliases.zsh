1905c7aafbb7272a36f832383c4ccf3261f15263
James Conroy-Finn <james@logi.cl>
2012-09-15 23:27:35 +0100

    Add aliases for working with Zeus

--- ruby/aliases.zsh
+++ ruby/aliases.zsh
@@ -15,3 +15,14 @@ alias migrate='rake db:migrate db:test:clone_structure'
 
 # Tail the test log, ignoring TRUNCATE output from database_cleaner
 alias rlog='tail -f log/test.log | grep -v TRUNCATE'
+
+# Zeus commands
+alias z='zeus start'
+alias zg='zeus generate'
+alias zs='zeus server'
+alias zc='zeus console'
+alias zr='zeus rspec'
+alias zk='zeus cucumber'
+alias zrn='zeus runner'
+alias zdb='zeus dbconsole'
+alias zrake='zeus rake'
