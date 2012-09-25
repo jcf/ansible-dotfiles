f7ef74f3eb955df9a1f3935a1a7d414755aaf1d2
James Conroy-Finn <james@logi.cl>
2012-09-25 09:56:28 +0100

    Bring -d32 back to JAVA_OPTS

--- jruby/options.zsh
+++ jruby/options.zsh
@@ -3,3 +3,5 @@ if [[ -d "/Library/Java/JavaVirtualMachines/jdk1.7.0_07.jdk/Contents/Home" ]]; t
   export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_07.jdk/Contents/Home"
   path=($JAVA_HOME/bin $path)
 fi
+
+export JAVA_OPTS='-d32'
