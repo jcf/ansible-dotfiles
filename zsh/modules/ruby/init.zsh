a4758014dff7ba6c2ac8224e48c9666435b2073b
James Conroy-Finn <james@logi.cl>
2012-12-03 10:10:47 +0000

    Move functions around
    - Move Rails functions and aliases from Ruby in to Rails.
    - Remove h, savepath, and timeline functions.
    - Compile ZSH config files in their new locations.

--- zsh/modules/ruby/init.zsh
+++ zsh/modules/ruby/init.zsh
@@ -94,26 +94,12 @@ alias bs='bundle show'
 # Start Guard using bundle exec
 alias beg='bundle exec guard'
 
-# Migrate and clone structure in to test database
-alias migrate='rake db:migrate db:test:clone_structure'
-
 # Tail the test log, ignoring TRUNCATE output from database_cleaner
 alias rlog='tail -f log/test.log | grep -v TRUNCATE | grep -v "ALTER TABLE"'
 
 # Heroku aliases
 alias hc='heroku console'
 alias hr='heroku run'
-
-# Zeus commands
-alias z='zeus start'
-alias zg='zeus generate'
-alias zs='zeus server'
-alias zc='zeus console'
-alias zr='zeus rspec'
-alias zk='zeus cucumber'
-alias zrn='zeus runner'
-alias zdb='zeus dbconsole'
-alias zrake='zeus rake'
 
 # Use Nailgun to run some Ruby
 alias ng='ruby --ng -S'
