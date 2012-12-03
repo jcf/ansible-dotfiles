985bb983295e5f17b3ee7172032a68e2bde9f08e
James Conroy-Finn <james@logi.cl>
2012-12-03 10:45:23 +0000

    Use my Rails aliases

--- zsh/modules/rails/init.zsh
+++ zsh/modules/rails/init.zsh
@@ -16,20 +16,20 @@ fi
 # Aliases (Compatible with Rails 2)
 #
 
-alias ror='rails'
-alias rorc='_rails-command console'
-alias rordc='_rails-command dbconsole'
-alias rordm='rake db:migrate'
-alias rordM='rake db:migrate db:test:clone'
-alias rordr='rake db:rollback'
-alias rorg='_rails-command generate'
-alias rorl='tail -f log/development.log'
-alias rorlc='rake log:clear'
-alias rorp='_rails-command plugin'
-alias rorr='_rails-command runner'
-alias rors='_rails-command server'
-alias rorsd='_rails-command server --debugger'
-alias rorx='_rails-command destroy'
+alias r='rails'
+alias rc='_rails-command console'
+alias rdc='_rails-command dbconsole'
+alias rdm='rake db:migrate'
+alias rdM='rake db:migrate db:test:clone'
+alias rdr='rake db:rollback'
+alias rg='_rails-command generate'
+alias rl='tail -f log/development.log'
+alias rlc='rake log:clear'
+alias rp='_rails-command plugin'
+alias rr='_rails-command runner'
+alias rs='_rails-command server'
+alias rsd='_rails-command server --debugger'
+alias rx='_rails-command destroy'
 
 # Migrate and clone structure in to test database
 alias migrate='rake db:migrate db:test:clone_structure'
@@ -56,4 +56,3 @@ function _rails-command {
     ruby script/rails "$@"
   fi
 }
-
