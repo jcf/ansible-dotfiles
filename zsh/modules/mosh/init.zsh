ad7074e6b7805dad50a8f0811a8fa5f0d71dc234
James Conroy-Finn <james@logi.cl>
2012-12-17 12:46:52 +0000

    Remove bosh alias

--- zsh/modules/mosh/init.zsh
+++
@@ -1,7 +0,0 @@
-if (( ! $+commands[mosh] )); then
-  return 1
-fi
-
-# Useful when you've installed mosh using Brew and it mosh-server isn't in your
-# PATH when you connect.
-alias bosh="mosh --server=/usr/local/bin/mosh-server"
