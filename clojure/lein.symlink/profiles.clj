1ada68ee61820bfad90ce48be787dfc8eb5072fe
James Conroy-Finn <james@logi.cl>
2012-12-26 15:58:04 +0000

    Add Leiningen config

---
+++ clojure/lein.symlink/profiles.clj
@@ -0,0 +1,5 @@
+{:user {:plugins [[lein-difftest "1.3.8"]
+                  [lein-marginalia "0.7.1"]
+                  [lein-pprint "1.1.1"]
+                  [lein-swank "1.4.4"]]
+        :search-page-size 30}}
