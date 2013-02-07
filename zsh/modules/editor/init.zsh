bff9f4688e454c5ea9caed1b9f08ef64185a3ea7
James Conroy-Finn <james@logi.cl>
2013-02-07 14:57:36 +0000

    Set empty keys to an invalid UTF-8 sequence

--- zsh/modules/editor/init.zsh
+++ zsh/modules/editor/init.zsh
@@ -90,12 +90,11 @@ key_info=(
   'BackTab'   "$terminfo[kcbt]"
 )
 
-# Do not bind any keys if there are empty values in $key_info.
+# Set empty $key_info values to an invalid UTF-8 sequence to induce silent
+# bindkey failure.
 for key in "${(k)key_info[@]}"; do
   if [[ -z "$key_info[$key]" ]]; then
-    print "prezto: one or more keys are non-bindable" >&2
-    unset key{,_info}
-    return 1
+    key_info["$key"]='�'
   fi
 done
 
@@ -269,10 +268,6 @@ bindkey -M vicmd "v" edit-command-line
 # Undo/Redo
 bindkey -M vicmd "u" undo
 bindkey -M vicmd "$key_info[Control]R" redo
-
-# Switch to command mode.
-bindkey -M viins "jk" vi-cmd-mode
-bindkey -M viins "kj" vi-cmd-mode
 
 if (( $+widgets[history-incremental-pattern-search-backward] )); then
   bindkey -M vicmd "?" history-incremental-pattern-search-backward
