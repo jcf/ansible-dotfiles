22afbd44b7a3013cafb8358d616aeacdd1260a24
James Conroy-Finn <james@logi.cl>
2013-09-26 14:49:19 +0100

    Add slamhound to lein plugins

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -1,5 +1,6 @@
 {:user {:plugins [[lein-difftest "1.3.8"]
                   [lein-marginalia "0.7.1"]
                   [lein-pprint "1.1.1"]
+                  [slamhound "1.3.1"]
                   [lein-swank "1.4.4"]]
         :search-page-size 30}}
