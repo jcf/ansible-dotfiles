a8e11871aed7e512463a32f187bc8b5f5cef2673
James Conroy-Finn <james@logi.cl>
2013-11-11 11:49:05 +0000

    Upgrade Clojure+leiningen support

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -1,6 +1,9 @@
-{:user {:plugins [[lein-difftest "1.3.8"]
+{:user {:dependencies [[slamhound "1.3.1"]
+                       [org.clojure/tools.namespace "0.2.4"]]
+        :plugins [[lein-difftest "1.3.8"]
                   [lein-marginalia "0.7.1"]
                   [lein-pprint "1.1.1"]
-                  [slamhound "1.3.1"]
                   [lein-swank "1.4.4"]]
+        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
+        :source-paths ["/Users/jcf/.lein/dev"]
         :search-page-size 30}}
