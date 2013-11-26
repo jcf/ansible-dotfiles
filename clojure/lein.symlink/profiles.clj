defe96a100ddedde056014d7e007deb69996ef49
James Conroy-Finn <james@logi.cl>
2013-11-26 15:23:33 +0000

    Add lein-kibit

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -3,7 +3,8 @@
         :plugins [[lein-difftest "1.3.8"]
                   [lein-marginalia "0.7.1"]
                   [lein-pprint "1.1.1"]
-                  [lein-swank "1.4.4"]]
+                  [lein-swank "1.4.4"]
+                  [lein-kibit "0.0.8"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :source-paths ["/Users/jcf/.lein/dev"]
         :search-page-size 30}}
