59376f7c3c44afff4a1d571013c492a7b424e350
James Conroy-Finn <james@logi.cl>
2014-02-08 19:29:04 +0000

    Default JRuby to Ruby 2 mode

--- emacs/emacs.d.symlink/init.el
+++ emacs/emacs.d.symlink/init.el
@@ -215,7 +215,7 @@
             ad-do-it)))))
   :config
   (progn
-    (setenv "JRUBY_OPTS" "--1.9")
+    (setenv "JRUBY_OPTS" "--2.0")
     (setenv "JAVA_OPTS" "-d32")
     (add-hook 'ruby-mode-hook 'rspec-mode)
     (add-hook 'ruby-mode-hook 'rbenv-use-corresponding)
