b8e6c3130736c3b776bfbfc14a6d12df8b9489a1
James Conroy-Finn <james@logi.cl>
2014-03-15 19:06:43 +0000

    Update Leiningen user profile
    - Setup Alembic
    - Add lein-ancient
    - Change page size to 50

--- clojure/lein.symlink/profiles.clj
+++ clojure/lein.symlink/profiles.clj
@@ -1,13 +1,15 @@
 {:user {:signing {:gpg-key "8ED1CE42"}
 
-        :dependencies [[clj-stacktrace "0.2.7"]
+        :dependencies [[alembic "0.2.1"]
+                       [clj-stacktrace "0.2.7"]
+                       [criterium "0.4.2"]
                        [org.clojure/tools.namespace "0.2.4"]
                        [slamhound "1.3.1"]
-                       [spyscope "0.1.4"]
-                       [criterium "0.4.2"]]
+                       [spyscope "0.1.4"]]
 
         :plugins [[codox "0.6.6"]
                   [jonase/eastwood "0.0.2"]
+                  [lein-ancient "0.5.4"]
                   [lein-cljsbuild "1.0.0"]
                   [lein-clojars "0.9.1"]
                   [lein-cloverage "1.0.2"]
@@ -17,10 +19,12 @@
                   [lein-pprint "1.1.1"]
                   [lein-swank "1.4.4"]]
 
-        :injections [(require 'spyscope.core
-                              '[clojure.repl :refer [source]]
-                              '[clojure.tools.namespace.repl :as repl]
-                              '[criterium.core :refer [bench quick-bench]])]
+        :injections [(require
+                      '[alembic.still :refer [distill]]
+                      '[clojure.repl :refer [source]]
+                      '[clojure.tools.namespace.repl :as repl]
+                      '[criterium.core :refer [bench quick-bench]]
+                      'spyscope.core)]
 
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
-        :search-page-size 30}}
+        :search-page-size 50}}
