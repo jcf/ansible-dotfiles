3ac5752b8a7db2494b248c2875c538499c81fc60
James Conroy-Finn <james@logi.cl>
2014-03-24 19:22:28 +0000

    Use ESC to quit lots of Emacs commands

--- emacs/emacs.d.symlink/evil-custom.el
+++ emacs/emacs.d.symlink/evil-custom.el
@@ -49,6 +49,15 @@
                (wdired-mode . normal))
           do (evil-set-initial-state mode state))
 
+    ;; Use escape to quit, and not as a meta-key.
+    (define-key evil-normal-state-map [escape] 'keyboard-quit)
+    (define-key evil-visual-state-map [escape] 'keyboard-quit)
+    (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+    (define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+    (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+    (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+    (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+
     (use-package evil-leader
       :init (global-evil-leader-mode)
       :config
