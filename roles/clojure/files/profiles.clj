f0f2aa8c3b67cae023b80c1ea10af2e2202c6e8a
James Conroy-Finn <james@logi.cl>
2015-02-19 12:31:22 +0000

    Use lein-shorthand to group my favourite functions

--- roles/clojure/files/profiles.clj
+++ roles/clojure/files/profiles.clj
@@ -20,17 +20,30 @@
                   [lein-kibit "0.0.8"]
                   [lein-marginalia "0.7.1"]
                   [lein-pprint "1.1.1"]
+                  [com.palletops/lein-shorthand "0.4.0"]
                   [lein-swank "1.4.4"]
                   [lein-try "0.4.3"]
                   [lein-typed "0.3.5"]
                   [refactor-nrepl "0.2.2"]]
 
-        :injections [(require
-                      '[alembic.still :refer [distill]]
-                      '[clojure.repl :refer [doc source]]
-                      '[clojure.tools.namespace.repl :as repl]
-                      '[criterium.core :refer [bench quick-bench]]
-                      'spyscope.core)]
+        :injections [(require 'spyscope.core)]
+
+        :shorthand {. [^:lazy alembic.still/distill
+                       ^:lazy ^:macro alembic.still/lein
+                       clojure.java.shell/sh
+                       clojure.pprint/pprint
+                       clojure.repl/apropos
+                       clojure.repl/dir
+                       clojure.repl/doc
+                       clojure.repl/find-doc
+                       clojure.repl/pst
+                       clojure.repl/source
+                       clojure.test/run-all-tests
+                       clojure.test/run-tests
+                       clojure.tools.namespace.repl/refresh
+                       clojure.tools.namespace.repl/refresh-all
+                       criterium.core/bench
+                       criterium.core/quick-bench]}
 
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :search-page-size 50}}
